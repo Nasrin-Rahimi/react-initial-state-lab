9dbc4220b9a5768a816576e52788e64081dc4ba4
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-03-24 07:04:20 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -9,7 +9,7 @@ export default class Bomb extends React.Component{
         }
     }
 
-    increment = () => {
+    decrement = () => {
         const newSecondsLeft = this.state.secondsLeft - 1 
         this.setState({
             secondsLeft: newSecondsLeft
@@ -18,7 +18,9 @@ export default class Bomb extends React.Component{
 
     render(){
         return (
-            this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft + ' seconds left before I go boom!'
+            <div onClick={this.decrement} >
+                {this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft + ' seconds left before I go boom!'}
+            </div>
         );
     }
 }
